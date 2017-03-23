c7eb157c845ea21d4f507773d21dd3647c58b286
David Eads <davideads@gmail.com>
2017-03-23 00:00:04 -0400

    the worst

--- tiler.py
+++ tiler.py
@@ -8,8 +8,8 @@ from cropped_thumbnail import cropped_thumbnail
 
 BASE_WIDTH = 450
 BASE_HEIGHT = 550
-IMAGE_COLUMNS = 9
-ASPECT_RATIO = 16/9.
+IMAGE_COLUMNS = 8
+ASPECT_RATIO = 1.7
 DATA_URL = 'http://staging.project.wnyc.org/healthcare-stance/assets/data/all-states.json'
 LOG_FORMAT = '%(levelname)s:%(name)s:%(asctime)s: %(message)s'
 URL_TEMPLATE = 'https://theunitedstates.io/images/congress/{0}x{1}/%s.jpg'.format(BASE_WIDTH, BASE_HEIGHT)
@@ -21,8 +21,8 @@ logger.setLevel(logging.INFO)
 
 def get_ids():
     resp = requests.get(DATA_URL)
-    ids = [row['bioguideid'] for row in resp.json() if row.get('edited')]
-    return ids
+    ids = [row['bioguideid'] for row in resp.json() if (row.get('edited') and row.get('party') == 'R' and row.get('voting_member') == 'TRUE' and row.get('position') == 'Opposed') and row.get('chamber') == 'house']
+    return sorted(ids)
 
 
 def calculate_rows():
@@ -57,10 +57,14 @@ def make_promotion_thumb():
         j = 0
 
         while j < IMAGE_COLUMNS:
+            if seen == ids:
+                break
+
             id = random.choice(ids)
             if id in seen:
                 continue
             seen.append(id)
+            seen = sorted(seen)
 
             imgpath = get_image(id)
 
@@ -81,7 +85,7 @@ def make_promotion_thumb():
 
     image.save('congress-uncropped.jpg', quality=95)
 
-    cropped = cropped_thumbnail(image, [4050, 2278])
+    cropped = cropped_thumbnail(image, [2000 * ASPECT_RATIO, 2000])
     cropped.save('congress.jpg', quality=95)
 
 
